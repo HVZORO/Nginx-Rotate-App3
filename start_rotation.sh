0bf025c9bf472b378f9b0766520fbca1b3c3dbbd
Harsha Vardhan <harshazoro24@gmail.com>
2026-07-01 10:39:39 +0000

    first commit

---
+++ start_rotation.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Stopping old rotation..."
+
+pkill -f rotate_pages.sh || true
+
+echo "Starting new rotation..."
+
+nohup /home/harsha_pc/nginx-rotate-app2/webpages/rotate_pages.sh \
+> /home/harsha_pc/nginx-rotate-app2/rotation.log 2>&1 &
+
+echo "Started"
+
---
+++ webpages/rotate_pages.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+TARGET="/var/www/html/index.html"
+SRC_DIR="/home/harsha_pc/nginx-rotate-app2/webpages"
+
+PAGES=("index1.html" "index2.html" "index3.html")
+
+DURATION=300
+INTERVAL=2
+
+END_TIME=$((SECONDS + DURATION))
+
+while [ $SECONDS -lt $END_TIME ]
+do
+  for page in "${PAGES[@]}"
+  do
+    ln -sf "$SRC_DIR/$page" "$TARGET"
+    sleep $INTERVAL
+
+    if [ $SECONDS -ge $END_TIME ]; then
+      break
+    fi
+  done
+done
